af5fc8652456a9182d1aaee5c366f13e2e30e63e
Shiv narayan sah <shivnarayan2072@gmail.com>
2025-09-01 12:55:55 +0545

    djbjd

--- src/api/mainApi.jsx
+++ src/api/mainApi.jsx
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 import dotenv from "dotenv";
 dotenv.config();
-export const BASE_URL = process.env.MAIN_API_URL;
+export const BASE_URL = "https://coffee-house-backend.onrender.com";
 
 export const mainApi = createApi({
   reducerPath: "mainApi",
