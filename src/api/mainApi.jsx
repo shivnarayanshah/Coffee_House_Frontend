fb46a9843f89aad51e2e1da1b066a0b6ff1c8075
Shiv narayan sah <shivnarayan2072@gmail.com>
2025-09-01 12:51:47 +0545

    change in dotenv

--- src/api/mainApi.jsx
+++ src/api/mainApi.jsx
@@ -2,11 +2,12 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 import dotenv from "dotenv";
 dotenv.config();
+export const BASE_URL = process.env.MAIN_API_URL;
 
 export const mainApi = createApi({
   reducerPath: "mainApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `${process.env.MAIN_API_URL}/api`,
+    baseUrl: `${BASE_URL}/api`,
   }),
   endpoints: (builder) => ({}),
 });
